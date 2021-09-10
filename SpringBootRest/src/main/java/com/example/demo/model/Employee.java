069855abafd62debb4d9ae17ec7c9f44d5e32380
Sqqal <Sqqal@192.168.6.238>
2021-09-10 20:30:27 +0200

    First Commit

---
+++ SpringBootRest/src/main/java/com/example/demo/model/Employee.java
@@ -0,0 +1,44 @@
+package com.example.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+	@Id
+	@GeneratedValue
+	private int id;
+	private String name;
+	private String country;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", country=" + country + "]";
+	}
+	
+	
+	
+	
+	
+
+}
---
+++ SpringBootRest/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into EMPLOYEE (id, name, country) values (3 , 'jojo'   ,'brasil'   ); 
+insert into EMPLOYEE (id, name, country) values (4 , 'axel'  ,'uk'       ); 
+insert into EMPLOYEE (id, name, country) values (5 , 'lisa'  ,'italy'   ); 
+insert into EMPLOYEE (id, name, country) values (6 , 'david' ,'portugal' ); 
+insert into EMPLOYEE (id, name, country) values (7 , 'julie' ,'china'    );
